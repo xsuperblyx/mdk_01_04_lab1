6dbe4fe755b0267714538e74336def959e8f7d6f
Grigorii <grigoriievdokimov2002@gmail.com>
2021-11-23 19:38:47 +0300

    01

--- lb1var12/lb1var12.cpp
+++ lb1var12/lb1var12.cpp
@@ -1,20 +1,232 @@
-﻿// lb1var12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
 
-int main()
+class SqrMatrix
 {
-    std::cout << "Hello World!\n";
+private:
+	double** result;
+	double** matrix;
+	double** matrix2;
+	double** matrixres;
+	int size;
+
+public:
+	SqrMatrix(int s)
+	{
+		size = s;
+	}
+
+	double Creatematrix() // создание матриц
+	{
+		matrix = new double* [size];
+		matrix2 = new double* [size];
+		matrixres = new double* [size];
+		result = new double* [size];
+		for (int i = 0; i < size; i++)
+		{
+			matrix[i] = new double[size];
+			matrix2[i] = new double[size];
+			matrixres[i] = new double[size];
+			result[i] = new double[size];
+		}
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				matrix[i][j] = 0;
+				matrix2[i][j] = 0;
+				matrixres[i][j] = 0;
+			}
+		}
+		return **matrix;
+	}
+
+	void Fillmatrix() // заполнение матриц
+	{
+		cout << "1) Генерация матриц" << endl;
+
+		//первая матрица
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				matrix[i][j]= rand() % 3 + 1; // заполнение случайными числами от 1 до 3
+			}
+		}
+
+		//вторая матрица
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				matrix2[i][j] = rand() % 3 + 1; // заполнение случайными числами от 1 до 3
+			}
+		}
+
+		cout << "Сгенерированные матрицы размером " << size << "x" << size << ":" << endl;
+
+		//вывод первой матрицы
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				cout << matrix[i][j] << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+
+		// вывод второй матрицы
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				cout << matrix2[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+	void Scalarmult() // умножение на скаляр
+	{
+		double scalar;
+		cout << endl;
+		cout << "2) Умножение матрицы на скаляр" << endl;
+		cout << "Умножить матрицу на:" << endl;
+		cin >> scalar;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				result[i][j] = matrix[i][j] * scalar;
+			}
+		}
+		cout << "Результат умножения матрицы на скаляр:" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				cout << result[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+	void Matrixmult() // умножжение матриц
+	{
+		cout << endl;
+		cout << "3) Умножение матриц" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				for (int m = 0; m < size; m++)
+				{
+					matrixres[i][j] += matrix[i][m] * matrix2[m][j];
+				}
+			}
+		}
+
+		// вывод 
+		cout << "Результат умножения:" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				cout << matrixres[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+	void Matrixsum() // сложение матриц
+	{
+		cout << endl;
+		cout << "4) Сложение матриц" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+					matrixres[i][j] = matrix[i][j] + matrix2[i][j];
+			}
+		}
+
+		// вывод 
+		cout << "Результат сложения:" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				cout << matrixres[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+	void Matrixsub() // вычитание матриц
+	{
+		cout << endl;
+		cout << "5) Вычитание матриц" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				matrixres[i][j] = matrix[i][j] - matrix2[i][j];
+			}
+		}
+
+		// вывод 
+		cout << "Результат вычитания:" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				cout << matrixres[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+	void Matrixdiv() // деление матриц
+	{
+		cout << endl;
+		cout << "6) Деление матриц" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				for (int m = 0; m < size; m++)
+				{
+					matrixres[i][j] = 0;
+					matrixres[i][j] += matrix[i][m] * (1/matrix2[m][j]);
+				}
+			}
+		}
+
+		// вывод 
+		cout << "Результат деления:" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				cout << matrixres[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+};
+
+
+void main()
+{
+	setlocale(0, "RUSSIAN");
+	SqrMatrix mat(5);
+	mat.Creatematrix();
+	mat.Fillmatrix();
+	mat.Scalarmult();
+	mat.Matrixmult();
+	mat.Matrixsum();
+	mat.Matrixsub();
+	mat.Matrixdiv();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
